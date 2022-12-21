110d73c27783fe15af939a4ddc2e1f6c0cbb268f
tomas-cabrera <tcabrera@andrew.cmu.edu>
2022-12-21 18:58:50 -0500

    First version of proper motion histogram.  Awkward to load

--- src/scripts/generate_credible_regions.py
+++ src/scripts/generate_credible_regions.py
@@ -1,10 +1,12 @@
+import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import multiprocessing as mp
 import parmap
-from astropy.coordinates import SkyCoord, Galactocentric, ICRS
 from astropy import units as u
+from astropy.coordinates import SkyCoord, Galactocentric, ICRS
+from astropy.io import fits
 import healpy as hp
 
 import paths
@@ -14,7 +16,7 @@ import rustics
 
 
 def calc_credible_indices(probs, intervals):
-    """! Return an array of indices that compose the specified credible intervals.
+    """! Return boolean masks that compose the specified intervals.
 
     @type   probs:      np.array
     @param  probs:      Array of probabilities
@@ -30,43 +32,68 @@ def calc_credible_indices(probs, intervals):
     # Get sort indices, sort probabilities
     argsort_inds = np.argsort(probs)[::-1]
     probs_sorted = probs[argsort_inds]
-    print("probs_sorted:", probs_sorted)
 
     # Get cumulative probabilities, and find indices of intervals
     probs_cumsum = np.cumsum(probs_sorted)
     ss_inds = np.searchsorted(probs_cumsum, intervals, side="right")
-    print("ss_inds:", ss_inds)
 
     # Generate boolean masks for credible regions
-    masks = np.full((len(intervals), probs.shape[0]), False)
+    masks = np.full((len(intervals), probs.shape[0]), 0)
     for ii, i in enumerate(ss_inds):
-        masks[ii, argsort_inds[:i]] = True
-    print("masks:", masks)
-    print("probs[masks[0,:]]:", probs[masks[0, :]])
+        masks[ii, argsort_inds[:i]] = 1
+
+    return masks
+
+
+def calc_credible_indices_2d(probs, intervals):
+    """! Return boolean masks that compose the specified intervals, for a 2D array.
+
+    @type   probs:      np.array
+    @param  probs:      Array of probabilities
+
+    @type   intervals:  list
+    @param  intervals:  List of credible regions to calculate.
+
+    @rtype:             np.array
+    @return:            Boolean array that can be used to mask probabilities to credible region.
+
+    """
+
+    # Get sort indices, sort probabilities
+    argsort_inds = np.unravel_index(np.argsort(probs, axis=None)[::-1], probs.shape)
+    probs_sorted = probs[argsort_inds]
+
+    # Get cumulative probabilities, and find indices of intervals
+    probs_cumsum = np.cumsum(probs_sorted)
+    ss_inds = np.searchsorted(probs_cumsum, intervals, side="right")
+
+    # Generate boolean masks for credible regions
+    masks = np.full((len(intervals), *probs.shape), 0)
+    for ii, i in enumerate(ss_inds):
+        masks[ii, (argsort_inds[0][:i], argsort_inds[1][:i])] = 1
 
     return masks
 
 
 def calc_ej_credible_regions(
-    gc_name,
+    gc_path,
     intervals=[0.5, 0.9],
-    dir_path=paths.data_mwgcs,
     ej_fname="output_N-10_ejections.txt",
     hp_level=5,
     hp_fname="hp_probs.fits",
     pm_fname="pm_probs.fits",
     overwrite=True,
+    kw_writemap={
+        "nest": True,
+    },
 ):
     """! Calculate the credible regions for the ejections from one GC.
 
-    @type   gc_name:    string
-    @param  gc_name:    Name of globular cluster
+    @type   gc_path:    PosixPath
+    @param  gc_path:    Path to globular cluster directory.
 
     @type   intervals:  list
     @param  intervals:  List of floats of credible regions to calculate.
-
-    @type   dir_path:   string
-    @param  dir_path:   path/to/directory containing GC folders
 
     @type   ej_fname:   string
     @param  ej_fname:   Filename of ejections data.
@@ -75,28 +102,30 @@ def calc_ej_credible_regions(
     @param  hp_level:   Level/order of healpix map to use for spatial coordinates.
 
     @type   hp_fname:   string
-    @param  hp_fname:   Filename to use for HEALPix counts
+    @param  hp_fname:   Filename to use for HEALPix counts.  If None, then skips generating the HEALPix histograms.
 
     @type   pm_fname:   string
-    @param  pm_fname:   Filename to use for proper motion counts
+    @param  pm_fname:   Filename to use for proper motion countsi.  If None, then skips generating the histograms.
+
+    @type   kw_writemap:   dict
+    @param  kw_writemap:   Keyword arguments for hp.fitsfunc.write_map.
 
     @rtype:             None
     @return:            No return, just saves the file.
 
     """
 
+    # Choose columns to load
+    if (hp_fname == None) & (pm_fname == None):
+        raise Warning("Neither fname set, so no data will be read.")
+    usecols = ["X", "Y", "Z"]
+    if pm_fname != None:
+        usecols += ["U", "V", "W"]
+
     # Load ejection data; cut out problematic points
-    path = dir_path / gc_name / ej_fname
     ejdf = rustics.EjectionDf(
-        path,
-        usecols=[
-            "X",
-            "Y",
-            "Z",
-            "U",
-            "V",
-            "W",
-        ],
+        gc_path / ej_fname,
+        usecols=usecols,
     )
     ejdf.df = ejdf.df[(ejdf.df.X != -1) & (ejdf.df.Y != -1)]
 
@@ -115,55 +144,199 @@ def calc_ej_credible_regions(
     #####################
     ###   Positions   ###
     #####################
-    # HEALPix numbers
-    nside = hp.pixelfunc.order2nside(hp_level)
-    npix = hp.pixelfunc.nside2npix(nside)
+    if hp_fname != None:
+        # HEALPix numbers
+        nside = hp.pixelfunc.order2nside(hp_level)
+        npix = hp.pixelfunc.nside2npix(nside)
 
-    # Calculate HEALPixels for objects and count number of objects/hp
-    hps = hp.pixelfunc.ang2pix(
-        nside,
-        scej.ra.deg,
-        scej.dec.deg,
-        nest=True,
-        lonlat=True,
-    )
-    hp_probs = np.bincount(hps, minlength=npix)
-    hp_probs = hp_probs / hp_probs.sum()
+        # Calculate HEALPixels for objects and count number of objects/hp
+        hps = hp.pixelfunc.ang2pix(
+            nside,
+            scej.ra.deg,
+            scej.dec.deg,
+            nest=True,
+            lonlat=True,
+        )
+        hp_probs = np.bincount(hps, minlength=npix)
+        hp_probs = hp_probs / hp_probs.sum()
 
-    # Get lists of credible indices
-    credible_indices = calc_credible_indices(hp_probs, intervals)
+        # Get lists of credible indices
+        credible_indices = calc_credible_indices(hp_probs, intervals)
 
-    # Stack data and save to HEALPix fits file
-    output = (hp_probs, *credible_indices)
-    print(output)
-    print("hp.maptype:", hp.pixelfunc.maptype(output))
-    print("extra_header:")
-    hp.fitsfunc.write_map(
-        dir_path / gc_name / hp_fname,
-        output,
-        column_names=["PROB"] + ["CR%d" % (i * 100) for i in intervals],
-        dtype=[float] + [bool] * len(intervals),
-        nest=True,
-        overwrite=overwrite,
-        extra_header=[
-            ("NEJECT", ejdf.df.shape[0], "Total number of ejected objects"),
-        ],
-    )
+        # Stack data and save to HEALPix fits file
+        output = (hp_probs, *credible_indices)
+        hp.fitsfunc.write_map(
+            gc_path / hp_fname,
+            output,
+            column_names=["PROB"] + ["CR%d" % (i * 100) for i in intervals],
+            dtype=[float] + [bool] * len(intervals),
+            extra_header=[
+                ("NEJECT", ejdf.df.shape[0], "Total number of ejected objects"),
+            ],
+            overwrite=overwrite,
+            **kw_writemap,
+        )
 
     #####################
     ### Prop. motions ###
     #####################
+    # pm_dec and pm_ra_cosdec extrema (mas / yr):
+    #   -610.551 <=    pm_dec    <= 154.390
+    #   -430.575 <= pm_ra_cosdec <= 220.048
+    # Generally, >99.9% of the ejecta have pm_dec in [-45, 15] and pm_ra_cosdec in [-30, 30]
+    #   In the worst cases, >98% are within this cut
+    if pm_fname != None:
+        # Generate 2D histogram
+        pmdmin = -45
+        pmdmax = 15
+        pmrcdmin = -30
+        pmrcdmax = 30
+        pmnum = 121
+        xbinedges = np.linspace(pmrcdmin, pmrcdmax, pmnum)
+        ybinedges = np.linspace(pmdmin, pmdmax, pmnum)
+        pm_counts, _, _ = np.histogram2d(
+            scej.pm_ra_cosdec.value,
+            scej.pm_dec.value,
+            bins=[xbinedges, ybinedges],
+        )
+        probs = pm_counts / ejdf.df.shape[0]
 
-    ### TODO: Do this next, after checking if extra_header can be used at all
+        # Calculate additional header info
+        coverage = probs.sum()
+
+        # Calculate credible regions
+        credible_indices = calc_credible_indices_2d(probs, intervals)
+
+        # Compose HDUList and write
+        probs_header = fits.Header(
+            [
+                ("NEJECT", ejdf.df.shape[0], "Total number of ejected objects"),
+                (
+                    "COVERAGE",
+                    coverage,
+                    "Fraction of objects within the histogram domain",
+                ),
+                ("PMDMIN", pmdmin, "Lower bound of pm_dec cut [mas/yr]"),
+                ("PMDMAX", pmdmax, "Upper bound of pm_dec cut [mas/yr]"),
+                ("PMRCDMIN", pmrcdmin, "Lower bound of pm_ra_cosdec cut [mas/yr]"),
+                ("PMRCDMAX", pmrcdmax, "Upper bound of pm_ra_cosdec cut [mas/yr]"),
+                (
+                    "PMNUM",
+                    pmnum,
+                    "Number of bins used along each dimension + 1",
+                ),
+            ]
+        )
+        probs_hdu = fits.PrimaryHDU(probs, header=probs_header)
+        ci_hdus = [fits.ImageHDU(ci) for ci in credible_indices]
+        hdul = fits.HDUList([probs_hdu, *ci_hdus])
+        hdul.writeto(gc_path / pm_fname, overwrite=overwrite)
+
+    return 0
+
+
+def get_ej_credible_regions(
+    gc_path,
+    hp_fname="hp_probs.fits",
+    pm_fname="pm_probs.fits",
+    generate=False,
+    kw_cecr={},
+    kw_readmap={
+        "h": True,
+        "field": None,
+    },
+):
+    """! Get the credible regions for the ejections from one GC.
+    If the regions are not pre-calculated, then generate them.
+
+    @type   gc_path:    PosixPath
+    @param  gc_path:    Path to globular cluster directory.
+
+    @type   hp_fname:   string
+    @param  hp_fname:   Filename to use for HEALPix counts.  If None, then doesn't look for the HEALPix histograms.
+
+    @type   pm_fname:   string
+    @param  pm_fname:   Filename to use for proper motion counts.  If None, then doesn't look for the histograms.
+
+    @type   generate:   bool
+    @param  generate:   If true, then generate files regarless if they are present or not.
+
+    @type   kw_cecr:    dict
+    @param  kw_cecr:    Keyword arguments to pass to calc_ej_credible_regions.
+
+    @rtype:             list
+    @return:            List of results.
+                        Each item in the list corresponds to one of the histograms,
+                        and is a tuple of the form (header(list), data(ndarray)),
+                        with the appropriate fields not included if specified.
+
+    """
+
+    # Generate any missing histograms
+    if hp_fname != None:
+        hp_path = gc_path / hp_fname
+        if not os.path.exists(hp_path):
+            kw_cecr["hp_fname"] = hp_fname
+    if pm_fname != None:
+        pm_path = gc_path / pm_fname
+        if not os.path.exists(pm_path):
+            kw_cecr["pm_fname"] = pm_fname
+    if generate | ((hp_fname != None) | (pm_fname != None)):
+        calc_ej_credible_regions(gc_path, **kw_cecr)
+
+    # Initialize output
+    output = []
+
+    # Load HEALPix histogram if named
+    if hp_fname != None:
+        temp = hp.fitsfunc.read_map(hp_path, **kw_readmap)
+        output.append(temp)
+
+    # Load proper motion histogram if named
+    # TODO: figure out what to do about fits.open remaining open.
+    if pm_fname != None:
+        temp = fits.open(pm_path)
+        output.append(temp)
+
+    return output
 
 
 ###############################################################################
 
 
-calc_ej_credible_regions("Liller_1")
-path = paths.data_mwgcs / "Liller_1/hp_probs.fits"
-# This works, but returns it as a numpy array
-m, h = hp.fitsfunc.read_map(path, h=True, field=None)
-print(h)
-print(m.shape)
+test_path = paths.data_mwgcs / "Liller_1"
+output = get_ej_credible_regions(test_path, hp_fname=None)
+print(output.info())
+
+# Iterate over all GCs
+mp_input = [paths.data_mwgcs / gc_name for gc_name in os.listdir(paths.data_mwgcs)]
+mp_input = [mp_input[0]]
+nprocs = None
+if nprocs == 1:
+    parmap.map(
+        get_ej_credible_regions,
+        mp_input,
+        hp_fname=None,
+        generate=True,
+        pm_parallel=False,
+        pm_pbar=True,
+    )
+else:
+    pool = mp.Pool(nprocs)
+    output = parmap.map(
+        get_ej_credible_regions,
+        mp_input,
+        hp_fname=None,
+        generate=True,
+        pm_pool=pool,
+        pm_pbar=True,
+    )
+output = np.array(output).reshape((len(output), 2))
+print(output)
+
+# Placeholder for showyourwork, atm.
+fig, axd = plt.subplot_mosaic([["0", "1"]])
+axd["0"].hist(output[:, 0])
+axd["1"].hist(output[:, 1])
+plt.tight_layout()
 plt.savefig(paths.figures / __file__.split("/")[-1].replace(".py", ".pdf"))
